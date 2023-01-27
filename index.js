eeef1151213e0911d42e06434d988e183bd53d03
Behruz01 <ibragimovbehruz822@gmail.com>
2023-01-27 18:30:41 +0500

    finish

--- index.js
+++ index.js
@@ -2,28 +2,46 @@ const elCards = document.querySelector("#cards");
 const elTemplate = document.querySelector("#template");
 const errorMassage = document.querySelector("#errorText");
 
-let todos = [];
+const elInput = document.querySelector("#inp");
+const names = document.querySelector("#name");
+const capital = document.querySelector("#capital");
+const population = document.querySelector("#population");
+const flag = document.querySelector("#flag");
+const weather = document.querySelector("#weather");
+const region = document.querySelector("#region");
 
-function renderPost(array, parent = elCards) {
-  parent.textContent = "";
-  array.forEach((todo) => {
-    const template = elTemplate.textContent.cloneNode(true);
-    const title = template.querySelector(".card-title");
-    const text = template.querySelector(".card-text");
+elInput.addEventListener("change", (evt) => {
+  const value = evt.target.value;
 
-    title.textContent = todo.name;
-    text.textContent = todo.capital;
-
-    parent.appendChild(template);
-  });
-}
-fetch("https://restcountries.com/v3.1/all")
-  .then((res) => res.json())
-  .then((data) => {
-    todos = data;
-    console.log(data);
-    renderPost(todos);
-  })
-  .catch((err) => {
-    errorMassage.textContent = "Ma'lumot topilmadi!";
-  });
+  function state() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=46351da790226c653537b9628dc20463`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        weather.textContent = data.main.feels_like;
+        console.log(data);
+      });
+    fetch(`
+    https://restcountries.com/v3.1/name/${value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        names.textContent = data[0].name.common;
+        capital.textContent = "Capital: " + data[0].capital;
+        population.textContent = "Population: " + data[0].population;
+        flag.textContent = "Flag: " + data[0].flag;
+        region.textContent = "Region" + data[0].subregion;
+      })
+      .catch((err) => {
+        errorMassage.textContent = "Ma'lumot topilmadi!";
+        capital.textContent = "";
+        flag.textContent = "";
+        population.textContent = "";
+        names.textContent = "";
+        region.textContent = "";
+        weather.textContent = "";
+      });
+  }
+  state();
+});
